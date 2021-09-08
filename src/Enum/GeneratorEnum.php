2946db9ff46bb2a7decd6126def086044ee4406d
welly-arnold kouya <welly-arnold.kouya@vigicorp.fr>
2021-09-08 10:52:15 +0200

    wip structured lib

---
+++ src/Enum/GeneratorEnum.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Berlioz\Generator\Command\Enum;
+
+class GeneratorEnum 
+{
+    const CONTROLLERS_PATH = 'src/Controller/';
+    const SERVICES_PATH = 'src/Service/';
+    const ENTITIES_PATH = 'src/Domain/Entity/';
+    const REPOSITORIES_PATH = 'src/Domain/Entity/';
+    const VIEWS_PATH = 'templates/';
+
+    const VALID_CHOICE = [0,1,2,3,4];
+}
